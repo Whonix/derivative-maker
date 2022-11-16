cfa7a8775d568b555f83bb0de7778de2f8037ea5
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2022-11-15 20:01:06 -0700

    Add notes

--- automated_builder/scripts/run_wats.sh
+++ automated_builder/scripts/run_wats.sh
@@ -7,21 +7,22 @@ main() {
 }
 
 prepare_environment() {
-  echo changeme | sudo -S apt-get update 
+  echo changeme | sudo -S apt-get update -q
   sudo apt-get install git python3-behave python3-pip python3-pyatspi -yq
-  pip3 install dogtail
+  pip3 install dogtail -q
   gsettings set org.gnome.desktop.interface toolkit-accessibility true
 }
 
 install_source() {
   cd /home/user/
-  find -type d -name "whonix_automated_test_suite" -delete
+  # find -type d -name "whonix_automated_test_suite" -delete
   git clone https://github.com/Mycobee/whonix_automated_test_suite.git
 }
 
 run_tests() {
   cd whonix_automated_test_suite
-  DISPAY=0:0 behave ./features
+  DISPLAY=:0 xhost +
+  NO_AT_BRIDGE=1 DISPLAY=:0 behave ./features
 }
 
 main
