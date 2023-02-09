69848a5a6a92603d9660f68d127035c9de0d627c
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-02-09 15:06:19 -0700

    Extend sleep time for Tor connection

--- automated_builder/roles/gui-build/templates/run_wats.sh
+++ automated_builder/roles/gui-build/templates/run_wats.sh
@@ -9,7 +9,9 @@ main() {
 prepare_environment() {
   dsudo setup-dist-noninteractive 1
   echo "Sleeping while Tor connects"
-  sleep 3m
+  echo "Time before sleep:" $(date)
+  sleep 5m
+  echo "Time after sleep:" $(date)
   dsudo apt-get update -q
   dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
   gsettings set org.gnome.desktop.interface toolkit-accessibility true
