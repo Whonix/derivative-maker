7ad59a51938d22e096b38af9e155bbb287591d68
Patrick Schleizer <adrelanos@whonix.org>
2023-02-09 11:35:06 -0500

    use dsudo

--- automated_builder/roles/gui-build/templates/run_wats.sh
+++ automated_builder/roles/gui-build/templates/run_wats.sh
@@ -7,8 +7,8 @@ main() {
 }
 
 prepare_environment() {
-  echo changeme | sudo -S apt-get update -q
-  sudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
+  dsudo -S apt-get update -q
+  dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
   gsettings set org.gnome.desktop.interface toolkit-accessibility true
 }
 
