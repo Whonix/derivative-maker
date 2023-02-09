3054f3954a7bb528cc95022e3247ef2cf40f85b8
Patrick Schleizer <adrelanos@whonix.org>
2023-02-09 11:31:18 -0500

    apt-get --no-install-recommends

--- automated_builder/roles/gui-build/templates/run_wats.sh
+++ automated_builder/roles/gui-build/templates/run_wats.sh
@@ -8,7 +8,7 @@ main() {
 
 prepare_environment() {
   echo changeme | sudo -S apt-get update -q
-  sudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq
+  sudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
   gsettings set org.gnome.desktop.interface toolkit-accessibility true
 }
 
