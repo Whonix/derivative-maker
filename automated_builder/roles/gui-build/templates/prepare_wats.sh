50f3594d5422aebe7233dfb96e2e3819a70b5e37
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-02-14 15:14:59 -0700

    Add DBUS_SESSION_BUS_ADDRESS for wats preparation

--- automated_builder/roles/gui-build/templates/prepare_wats.sh
+++ automated_builder/roles/gui-build/templates/prepare_wats.sh
@@ -8,7 +8,7 @@ main() {
 prepare_environment() {
   dsudo apt-get update -q
   dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
-  dsudo -E -u user gsettings set org.gnome.desktop.interface toolkit-accessibility true
+  dsudo -H -u user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.interface toolkit-accessibility true
 }
 
 install_source() {
