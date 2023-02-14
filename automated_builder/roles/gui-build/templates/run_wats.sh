1d9237aa9a789a8a4be4760a9194c0a05ecde597
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-02-14 10:43:57 -0700

    Tweak gsettings logic and log gathering syntax

--- automated_builder/roles/gui-build/templates/run_wats.sh
+++ automated_builder/roles/gui-build/templates/run_wats.sh
@@ -10,6 +10,7 @@ prepare_environment() {
   dsudo apt-get update -q
   dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
   gsettings set org.gnome.desktop.interface toolkit-accessibility true
+  dsudo systemctl daemon-reload
 }
 
 install_source() {
