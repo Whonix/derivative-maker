4eb101181dd80b4f1964276e232028288ae88365
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-02-14 12:52:34 -0700

    Move wats preparation to seperate script

---
+++ automated_builder/roles/gui-build/templates/prepare_wats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+main() {
+  prepare_environment
+  install_source
+}
+
+prepare_environment() {
+  dsudo apt-get update -q
+  dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
+  gsettings set org.gnome.desktop.interface toolkit-accessibility true
+}
+
+install_source() {
+  cd /home/user/
+  git clone https://github.com/Mycobee/whonix_automated_test_suite.git
+}
+
+main
--- automated_builder/roles/gui-build/templates/run_wats.sh
+++ automated_builder/roles/gui-build/templates/run_wats.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 main() {
-  prepare_environment
-  install_source
   run_tests
 }
 
-prepare_environment() {
-  dsudo apt-get update -q
-  dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
-  gsettings set org.gnome.desktop.interface toolkit-accessibility true
-  dsudo systemctl daemon-reload
-}
-
-install_source() {
-  cd /home/user/
-  git clone https://github.com/Mycobee/whonix_automated_test_suite.git
-}
-
 run_tests() {
-  cd whonix_automated_test_suite
+  cd /home/user/whonix_automated_test_suite
   DISPLAY=:0 xhost +
   NO_AT_BRIDGE=1 DISPLAY=:0 behave ./features
 }
