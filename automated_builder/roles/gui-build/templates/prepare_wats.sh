b09aca77765dd7b6f9fd6d520c7cf45f2a5c5545
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2023-02-15 15:10:36 -0700

    Add chmod for symlink

--- automated_builder/roles/gui-build/templates/prepare_wats.sh
+++ automated_builder/roles/gui-build/templates/prepare_wats.sh
@@ -9,6 +9,7 @@ prepare_environment() {
   dsudo apt-get update -q
   dsudo apt-get install git python3-behave python3-pip python3-pyatspi python3-dogtail -yq --no-install-recommends
   dsudo -H -u user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.interface toolkit-accessibility true
+  dsudo chmod 744 /etc/python3*
 }
 
 install_source() {
